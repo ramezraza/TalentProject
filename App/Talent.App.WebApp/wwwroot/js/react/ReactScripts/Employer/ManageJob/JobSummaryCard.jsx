e5e62b1d46615c079a41fcfab8278cde05f273f1
RAMESH NALLAMUTHU <ramezraza@gmail.com>
2023-08-25 00:10:45 +1000

    Add project files.

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -0,0 +1,136 @@
+﻿import React from 'react';
+import Cookies from 'js-cookie';
+import { Popup } from 'semantic-ui-react';
+import moment from 'moment';
+import { Pagination, Card, Button } from 'semantic-ui-react';
+
+export class JobSummaryCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+        const jobs = props.jobs ?
+            Object.assign({}, props.jobs)
+            : {
+                id: ''
+            }
+
+        this.state = {
+            MyJobs: jobs,
+            showJobs: false
+        }
+
+        this.loadJobs = this.loadJobs.bind(this)
+        this.selectJob = this.selectJob.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+        this.renderJobs = this.renderJobs.bind(this)
+    }
+
+
+
+
+    selectJob(id) {
+        var cookies = Cookies.get('talentAuthToken');
+        //url: 'http://localhost:51689/listing/listing/closeJob',
+
+    }
+
+
+
+    loadJobs() {
+
+        console.log("data", this.props.jobs)
+        const jobs = Object.assign({}, this.props.jobs)
+        console.log("jobs are", jobs)
+        const employerJobs = Object.values(jobs)
+
+
+
+        console.log("employerJobs", employerJobs);
+
+        if (employerJobs.length == 0) { alert("No Jobs Available") }
+        else {
+            this.setState({
+                MyJobs: employerJobs,
+                showJobs: true
+            })
+
+        }
+
+
+    }
+
+
+
+
+
+
+    render() {
+        return (
+
+
+            this.state.showJobs ? this.renderJobs() : this.renderDisplay()
+        )
+
+    }
+
+    renderJobs() {
+        return (
+            <div className='row'>
+                <div className="ui sixteen wide column">
+
+
+                    {/* {JSON.stringify(this.props.jobs) }*/}
+
+                    <Card.Group>
+
+                        {this.state.MyJobs.map(job =>
+
+                            <Card color='violet'>
+                                <Card.Content>
+                                    <Card.Header>Job Title : {job.title}</Card.Header>
+                                    <Card.Meta> Job Summary : {job.summary}</Card.Meta>
+
+                                </Card.Content>
+                                <Card.Content extra>
+                                    <Card.Description> Job Location : {job.location.city}</Card.Description>
+                                    <Card.Description>Country : {job.location.country}</Card.Description>
+                                </Card.Content>
+
+                                <Card.Content extra>
+                                    <Card.Meta> Status : {job.status} </Card.Meta>
+                                    <Card.Meta>No Of Suggestions : {job.noOfSuggestions}</Card.Meta>
+
+                                </Card.Content>
+                                <Card.Content extra>
+                                    <Button basic color='blue'>
+                                        Update Job
+                                    </Button>
+                                    <Button basic color='red'>
+                                        Close Job
+                                    </Button>
+                                </Card.Content>
+                            </Card>
+
+                        )}
+
+                    </Card.Group>
+                    <h1>  </h1>
+                </div>
+            </div>
+        )
+    }
+
+    renderDisplay() {
+
+
+        return (
+
+            <button type="button" className="ui left floated teal button" onClick={this.loadJobs} >Show My Jobs</button>
+
+        )
+
+
+
+    }
+}
---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -0,0 +1,137 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
+import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
+import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
+import { JobSummaryCard } from './JobSummaryCard.jsx';
+import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
+import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment } from 'semantic-ui-react';
+
+export default class ManageJob extends React.Component {
+    constructor(props) {
+        super(props);
+        let loader = loaderData
+        loader.allowedUsers.push("Employer");
+        loader.allowedUsers.push("Recruiter");
+        console.log(loader)
+        this.state = {
+            loadJobs: [],
+            loaderData: loader,
+            activePage: 1,
+            sortBy: {
+                date: "desc"
+            },
+            filter: {
+                showActive: true,
+                showClosed: false,
+                showDraft: true,
+                showExpired: true,
+                showUnexpired: true
+            },
+            totalPages: 1,
+            activeIndex: ""
+        }
+        this.loadData = this.loadData.bind(this);
+        this.init = this.init.bind(this);
+        this.loadNewData = this.loadNewData.bind(this);
+        //your functions go here
+        this.updateJobs = this.updateJobs.bind(this);
+    };
+
+    init() {
+        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        loaderData.isLoading = false;
+        this.setState({ loaderData });//comment this
+
+        //set loaderData.isLoading to false after getting data
+        //this.loadData(() =>
+        //    this.setState({ loaderData })
+        //)
+
+        console.log(this.state.loaderData)
+    }
+
+    componentDidMount() {
+        this.init();
+        this.loadData();
+    };
+
+    loadData() {
+        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
+        var cookies = Cookies.get('talentAuthToken');
+        // your ajax call and other logic goes here
+
+
+        $.ajax({
+            url: 'http://localhost:51689/listing/listing/getEmployerJobs',
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            success: function (res) {
+                console.log("respponse data", res)
+                let loadJobs = null;
+                if (res.myJobs) {
+                    loadJobs = res.myJobs
+
+                }
+                this.updateJobs(loadJobs)
+
+            }.bind(this),
+            error: function (res) {
+                console.log(res.status)
+            }
+        })
+        this.init()
+
+
+
+    }
+
+    updateJobs(myJobs) {
+
+        let jobs = Object.assign({}, this.state.loadJobs, myJobs)
+
+        this.setState({
+            loadJobs: jobs
+        })
+
+    }
+
+    loadNewData(data) {
+        var loader = this.state.loaderData;
+        loader.isLoading = true;
+        data[loaderData] = loader;
+        this.setState(data, () => {
+            this.loadData(() => {
+                loader.isLoading = false;
+                this.setState({
+                    loadData: loader
+                })
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+
+
+                <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+                    <div className="ui container">
+                        <JobSummaryCard
+
+                            jobs={this.state.loadJobs}
+                        />
+
+                    </div>
+                </BodyWrapper>
+
+
+            </div>
+        )
+    }
+}
